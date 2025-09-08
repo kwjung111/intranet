a7b4b34c5586e652501607d13f73425140bdfa72
kwjung <kwkwjung@gmail.com>
2025-09-09 00:21:14 +0900

    Fix: monitoring

---
+++ charts/network/ingress-nginx/install.sh
@@ -0,0 +1,5 @@
+echo "Apply CRDs..."
+
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.1/cert-manager.crds.yaml
+
+helm install ingress-nginx . -f values.yaml --namespace network --set installCRDs=true
